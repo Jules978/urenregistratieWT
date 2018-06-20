837992cd98c770e136855f918524d65bb5462e5b
Jules978 <vanDrunen.julia@gmail.com>
2018-06-20 13:23:17 +0200

    first commit

---
+++ src/test/java/com/urenregistratie/urenWT/UrenWtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.urenregistratie.urenWT;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UrenWtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
