517a671fa3905924dcd729b2a4ddceda0d2ed60c
Riordon Noredjo <r.noredjo@gmail.com>
2018-06-27 11:36:40 +0200

    first commit

---
+++ src/main/java/com/urenregistratie/urenWT/config/JerseyConfig.java
@@ -0,0 +1,17 @@
+package com.urenregistratie.urenWT.config;
+
+import com.urenregistratie.urenWT.api.UserEndpoint;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.ApplicationPath;
+
+
+@Component
+@ApplicationPath("/api")
+public class JerseyConfig extends ResourceConfig {
+	public JerseyConfig(){
+		register(UserEndpoint.class);
+
+	}
+}
