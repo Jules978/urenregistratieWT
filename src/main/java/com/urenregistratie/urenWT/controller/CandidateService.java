f2df54d40b27c9ce1724d307a3f716bf92a915b3
Riordon Noredjo <r.noredjo@gmail.com>
2018-06-29 15:08:03 +0200

    Endpoit Candidate' dsf

---
+++ src/main/java/com/urenregistratie/urenWT/controller/CandidateService.java
@@ -0,0 +1,35 @@
+package com.urenregistratie.urenWT.controller;
+
+import com.urenregistratie.urenWT.domain.Candidate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CandidateService {
+
+    @Autowired
+    CandidateRepository candidateRepository;
+
+    public Iterable<Candidate> giveAll (){
+        return candidateRepository.findAll();
+    }
+
+    public Candidate findById(Long id) {
+        return this.candidateRepository.findById(id).orElse(null);
+    }
+
+    public boolean existsById(Long id){
+        return this.candidateRepository.existsById(id);
+    }
+
+    public Candidate save(Candidate candidate){
+        return candidateRepository.save(candidate);
+    }
+
+    public void deleteById(Long id) {
+        this.candidateRepository.deleteById(id);
+        return;
+    }
+}
